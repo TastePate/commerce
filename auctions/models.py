ae8f6b34d2ea914d42429b17245056c4a3945efc
TastePate <pavelarz14@gmail.com>
2026-09-04 16:25:13 +0400

    update: changed Listing DB structure

--- auctions/models.py
+++ auctions/models.py
@@ -8,8 +8,9 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1500)
-    image_src = models.CharField(max_length=100, null=True, blank=True)
+    image_src = models.CharField(max_length=100, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    category = models.CharField(max_length=50, null=True)
     post_date = models.DateTimeField(auto_now_add=True)
     wishlisted_by = models.ManyToManyField(User, related_name="wishlisted_by")
     is_active = models.BooleanField(default=True)
