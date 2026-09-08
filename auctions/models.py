a00d6e0fb483d68db5de9c75f73be84f86504ecb
TastePate <pavelarz14@gmail.com>
2026-09-08 17:20:56 +0400

    update: category default value now 'Uncategorized'

--- auctions/models.py
+++ auctions/models.py
@@ -10,7 +10,7 @@ class Listing(models.Model):
     description = models.CharField(max_length=1500)
     image_src = models.CharField(max_length=100, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
-    category = models.CharField(max_length=50, null=True)
+    category = models.CharField(max_length=50, null=True, default="Uncategorized")
     post_date = models.DateTimeField(auto_now_add=True)
     start_amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     wishlisted_by = models.ManyToManyField(User, related_name="wishlisted_by")
