c27da8c3d52478352245a08aa09d5b519fd1b84a
TastePate <pavelarz14@gmail.com>
2026-09-10 15:25:41 +0400

    update: img link length now 1000

--- auctions/models.py
+++ auctions/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1500)
-    image_src = models.CharField(max_length=100, null=True)
+    image_src = models.CharField(max_length=1000, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     category = models.CharField(max_length=50, null=True, default="Uncategorized")
     post_date = models.DateTimeField(auto_now_add=True)
